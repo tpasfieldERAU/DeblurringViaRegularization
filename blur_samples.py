a6376259adf59739a994f96cd509207772856d1a
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2024-11-05 11:55:15 -0500

    Reduce imports on sample generator

--- blur_samples.py
+++ blur_samples.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy.sparse import csr_array, kron, eye_array
+from scipy.sparse import kron
 from scipy.stats import norm
 import scipy.linalg as la
-import matplotlib.pyplot as plt
 import cv2
 import os
 
